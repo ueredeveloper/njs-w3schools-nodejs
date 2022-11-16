f7a74c0582c05722ad50705663798164b5186d01
ueredeveloper <ueredeveloper@gmail.com>
2022-11-16 18:13:02 -0300

    exemplo pouco modificado

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var http = require('http');
  */
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end('Hello World - Get Started!');
 }).listen(8080);
