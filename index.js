85bf08e50c6cb605cf1e74e5fd4d3f975d9ac355
ueredeveloper <ueredeveloper@gmail.com>
2022-11-17 14:01:07 -0300

    Node.js Modules - criando um módulo próprio

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var dt = require('./myfirstmodule')
 
 /**
- * Primeiro exemplo. Também instalei o http (npm i http)
+ * Include your own module.
+ * Criação de um módulo próprio, no caso foi criado um arquivo `myfirsmodule.js` e importado no `index.js` para mostrar a data atual.
  */
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World - Get Started!');
+    res.end('The date time currently: ' + dt.myDateTime());
 }).listen(8080);
