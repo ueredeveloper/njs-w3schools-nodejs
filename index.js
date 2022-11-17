601bf35b4981102e2735c5ed862ee1565d559479
ueredeveloper <ueredeveloper@gmail.com>
2022-11-17 16:36:42 -0300

    Node.js como servidor

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /**
  * Módulo http integrado
+ * Node.js como servidor web
  */
 const http = require('http');
+
+http.createServer(function(req,res){
+    res.write('Hello World! - Node como servidor web');
+    res.end();
+
+}).listen(8080);
