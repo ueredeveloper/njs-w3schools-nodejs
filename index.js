159b243b2383d0071b3c6f79b24f4f4853989879
ueredeveloper <ueredeveloper@gmail.com>
2022-11-17 16:55:02 -0300

    ler string de consulta

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 /**
- * Módulo http integrado
- * Node.js como servidor web
+ * Módulo http integrado.
+ * Node.js como servidor web.
  * Incluindo cabeçalho.
+ * Lendo uma string de consulta.
  */
 const http = require('http');
 
 http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Adicionar cabeçalho HTTP.');
+    res.write(req.url);
     res.end();
 
 }).listen(8080);
