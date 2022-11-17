16f1d34014f01a0513847612391636b9806a7fa6
ueredeveloper <ueredeveloper@gmail.com>
2022-11-17 16:47:46 -0300

    adicionando cabeçalho http

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 /**
  * Módulo http integrado
  * Node.js como servidor web
+ * Incluindo cabeçalho.
  */
 const http = require('http');
 
 http.createServer(function(req,res){
-    res.write('Hello World! - Node como servidor web');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Adicionar cabeçalho HTTP.');
     res.end();
 
 }).listen(8080);
