ac410f0a6d1d5e3ec18888db618da654a337d1d5
ueredeveloper <ueredeveloper@gmail.com>
2022-11-17 17:27:06 -0300

    mudei o nome de um modulo para http-module

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var dt = require('./myfirstmodule')
 /**
  * Include your own module.
  * Criação de um módulo próprio, no caso foi criado um arquivo `myfirsmodule.js` e importado no `index.js` para mostrar a data atual.
+ * Renomeei um módulo para http-module.
  */
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/plain'});
