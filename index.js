428cd2c4434205b37f9737e5ea69c2a8957f7a5b
ueredeveloper <ueredeveloper@gmail.com>
2022-11-18 16:19:34 -0300

    renomeando arquivo .txt com rename - fs

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var http = require('http');
+var fs = require('fs');
 
 /**
  * Gettin started 
  */
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+
+  fs.rename("myfile.txt", "myfirstfile.txt", function(err){
+    if (err) throw err;
+    console.log('File renamed');
+  });
   res.end('Hello World!');
 }).listen(8080);
