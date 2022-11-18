6e35451138ef594fef90d942b24b94d4da17d44f
ueredeveloper <ueredeveloper@gmail.com>
2022-11-18 11:02:27 -0300

    fs - read files

--- README.md
+++ README.md
@@ -15,6 +15,9 @@ criação de um módulo próprio.
 ### Leituras
 
 [renomear branchs](https://stackoverflow.com/questions/30590083/how-do-i-rename-both-a-git-local-and-remote-branch-name)
+[tutoriais - git checkout] (https://www.atlassian.com/git/tutorials/using-branches/git-checkout#:~:text=The%20git%20branch%20command%20can,to%20switch%20to%20that%20branch.)
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
 var http = require('http');
 
 /**
- * Gettin started 
+ * Branch: file-system
+ * Node.js as file server
+ * Read files
  */
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+http.createServer(function(req, res){
+  fs.readFile('demofile1.html', function(err, data){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 }).listen(8080);
