ab3632006c74e678ec8b729c075bff94d80437d7
ueredeveloper <ueredeveloper@gmail.com>
2022-11-18 11:54:16 -0300

    criando um arquivo com open file

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ var http = require('http');
  */
 http.createServer(function(req, res){
 
-  // criando um arquivo no formato .txt
-  fs.appendFile('myfile1.txt', 'Hello Append File - Create File wiht fs', function(err){
+  // criando um arquivo txt com fs.open()
+  fs.open('openfile.txt', 'w', function(err, file){
     if (err) throw err;
-    console.log('Saved');
-  });
+    console.log('Saved!')
+  })
   // lendo o arquivo criado
-  fs.readFile('myfile1.txt', function(err, data){
+  fs.readFile('openfile.txt', function(err, data){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
