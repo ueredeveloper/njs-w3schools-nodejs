a649edaaec5edbf3b7973d3eabd7f1316a906926
ueredeveloper <ueredeveloper@gmail.com>
2022-11-18 10:08:14 -0300

    adicionado tag a para link de teste

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 /**
+ * branch: http-module
  * Módulo http integrado.
  * Node.js como servidor web.
  * Incluindo cabeçalho.
  * Lendo uma string de consulta.
+ * Dividindo uma string de consulta
  */
 const http = require('http');
 const url = require('url');
 
-http.createServer(function(req,res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    let q = url.parse(req.url, true).query;
-    let txt = q.year + " " + q.month;
-    
-    res.end(txt);
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  let q = url.parse(req.url, true).query;
+  let txt = "<h1>Node.js HTTP Module </h1>"
+    + "<a href='http://localhost:8080/?year=2023&month=11'>link de teste</a><br>"
+    + "<p> </p>"
+    + "ano: " + q.year + " mes: " + q.month;
+
+  res.end(txt);
 
 }).listen(8080);
