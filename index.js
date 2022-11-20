a3781aa2ec2a4a58c4aa8841eabf2db68918e327
Replit user <>
2022-11-20 07:47:48 +0000

    events in nodejs

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var http = require('http');
+var fs = require('fs');
 
 /**
  * Gettin started 
  */
 http.createServer(function (req, res) {
+
+  var rs = fs.createReadStream('./demofile.txt');
+  
+  rs.on('open', function(){
+    console.log("The file is open! ")
+  });
+  
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
 }).listen(8080);
