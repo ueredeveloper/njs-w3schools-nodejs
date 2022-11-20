31ff300741c76740e926e2bce37867356d0f38b4
Replit user <>
2022-11-20 06:54:22 +0000

    npm - using package

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var uc = require('upper-case');
 
 /**
  * Gettin started 
+ * adding upper-case
  */
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end(uc.upperCase('Hello World!'));
 }).listen(8080);
