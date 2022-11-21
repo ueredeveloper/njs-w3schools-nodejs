64c873ac3bed21b9212739f362b789388dbe4046
ueredeveloper <ueredeveloper@gmail.com>
2022-11-21 10:51:45 -0300

    adicionando novo branch events - events emiter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var events = require('events');
+const e = require('express');
 var eventEmiter = new events.EventEmitter();
 
 /**
@@ -8,11 +9,23 @@ var eventEmiter = new events.EventEmitter();
  *  Módulo embutido para criar, disparar e ouvir eventos.
  *  EventEmiter - todas as propriedades e métodos necessários.
  *    -> import events e criar uma variável eventEmiter.
+ * Events - EventEmiter
+ *  
  */
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+
+  var myEventEmiter = function (){
+    console.log('I hear e scream!')
+  }
+
+  eventEmiter.on('scream', myEventEmiter);
+
+  eventEmiter.emit('scream');
+
   res.end( `
     <h1> Branch Main </h1>
     <p> branch - events-module </p>
+    <p> branch - events-eventemiter </p>
   `);
 }).listen(8080);
