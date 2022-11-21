049d8f3f9cf2665b1a728fa22001f33953848218
ueredeveloper <ueredeveloper@gmail.com>
2022-11-21 10:36:37 -0300

    adicionando branch events-module

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var http = require('http');
+var events = require('events');
+var eventEmiter = new events.EventEmitter();
 
 /**
- * Gettin started 
+ * Gettin Started
+ * Events - Module
+ *  Módulo embutido para criar, disparar e ouvir eventos.
+ *  EventEmiter - todas as propriedades e métodos necessários.
+ *    -> import events e criar uma variável eventEmiter.
  */
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end( `
+    <h1> Branch Main </h1>
+    <p> branch - events-module </p>
+  `);
 }).listen(8080);
