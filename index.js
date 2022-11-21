1834e70c85cb1b4308297b817da504d562757bc8
ueredeveloper <ueredeveloper@gmail.com>
2022-11-21 18:24:48 -0300

    conexão criada mysql w3schools

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var http = require('http');
+var mysql = require('mysql');
+require('dotenv').config();
 
-/**
- * Gettin started 
- */
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+var con = mysql.createConnection({
+  host: process.env.MSYQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
