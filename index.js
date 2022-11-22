b620b6848e82ef1d78914a2e8d31b7666e7ddbb2
ueredeveloper <ueredeveloper@gmail.com>
2022-11-22 12:18:28 -0300

    ainda sem conexão

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
-const sql = require("msnodesqlv8");
 require('dotenv').config();
+var Connection = require('tedious').Connection;
 
-const connectionString = "server=.;Database=Master;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
-const query = "SELECT name FROM sys.databases";
-
-sql.query(connectionString, query, (err, rows) => {
-    console.log(rows);
+var config = {
+  server: process.env['SQLHOST'],
+  authentication: {
+    type: 'default',
+    options: {
+      userName: process.env['SQLUSERNAME'],
+      password: process.env['SQLPASSWORD'],
+    }
+  },
+  options: {
+ 
+    database: process.env['SQLHOST'],
+    port: 1433,
+    // If you are on Microsoft Azure, you need encryption:
+    //encrypt: true,
+  }
+};
+var connection = new Connection(config);
+connection.on('connect', function (err) {
+  if (err) {
+    console.error('error', err.message);
+  } else {
+    console.log('querydatabase')
+    queryDatabase();
+  }
+  connection.close();
 });
+
+connection.connect();
+
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+
+function queryDatabase() {
+  console.log("Reading rows from the Table...");
+
+  // Read all rows from table
+  const request = new Request(
+    `SELECT * FROM [dbo].[Colaborador]`,
+    (err, rowCount) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(`${rowCount} row(s) returned`);
+      }
+    }
+  );
+
+  request.on("row", columns => {
+    columns.forEach(column => {
+      console.log("%s\t%s", column.value);
+    });
+  });
+
+  connection.execSql(request);
+}
