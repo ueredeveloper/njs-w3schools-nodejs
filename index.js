b3583d058cfa1e9dc570b9b4f4b1a1f2b9fd01e1
ueredeveloper <ueredeveloper@gmail.com>
2022-11-23 16:00:50 -0300

    criando uma tabela

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 var mysql = require('mysql');
 require('dotenv').config();
 
+const {MYSQLHOST, MYSQLDATABASE, MYSQLUSERNAME, MYSQLPASSWORD} = process.env;
+
+console.log(MYSQLHOST, MYSQLDATABASE, MYSQLUSERNAME, MYSQLPASSWORD);
+
 var con = mysql.createConnection({
-  host: process.env.MSYQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD
+  host: MYSQLHOST,
+  user: MYSQLUSERNAME,
+  password: MYSQLPASSWORD,
+  database: MYSQLDATABASE
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+
+  var sql = "CREATE TABLE customers (name VARCHAR(205), address VARCHAR(255))";
+
+  con.query(sql, function(err, result){
+    if(err) throw err;
+
+    console.log("Table created!")
+  })
+
+
 });
