fd9bc487917fe5ebfdb889f7ed201d0e940f3c8a
ueredeveloper <ueredeveloper@gmail.com>
2022-11-23 16:31:57 -0300

    separando queries criar,banco, criar coluna e editar coluna

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ con.connect(function (err) {
   console.log("Connected!");
   // se a tabela estiver criada, deletá-la primeiro DROP TABLE customers
   //var sql = `DROP TABLE customers`
+  
   // criar tabela sem id
   //var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+
   // editar tabela adicionando id auto increment e com primary key
   var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY"
 
