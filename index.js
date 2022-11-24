77a946f4e41cf144076a874a530150fdf724a91b
ueredeveloper <ueredeveloper@gmail.com>
2022-11-24 13:28:21 -0300

    funcionando seleção contains dentro polígono e mostrar no mapa

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const { application } = require('express');
 var express = require('express');
 var app = express();
 require('dotenv').config();
+var path = require('path');
+var public = path.join(__dirname, 'public');
 
 const { ADASA_DATABASE, ADASA_USERNAME, ADASA_PASSWORD, ADASA_HOST } = process.env;
 
-app.get('/', function (req, res) {
+/*
+app.get('/', function(req, res) {
+   // res.sendFile(path.join(public, 'index.html'));
+});*/
+
+
+app.get('/getPoints', function (req, res) {
    
     var sql = require("mssql");
 
@@ -24,10 +33,12 @@ app.get('/', function (req, res) {
 
         // create Request object
         var request = new sql.Request();
+
+        let polygon = 'POLYGON((-47.73739483545068 -15.724018294609081,-47.50118878076318 -15.724018294609081 ,-47.502562071778804 -15.877300760051735 ,-47.74014141748193 -15.869375206281301,-47.73739483545068 -15.724018294609081))';
            
         // query to the database and get the records
-        request.query('select * from [gisadmin].[CONTATO]', function (err, recordset) {
-            
+        //request.query('select * from [gisadmin].[INTERFERENCIA] where ID_INTERFERENCIA < 2', function (err, recordset) {
+        request.query(`DECLARE @g geometry;SET @g = geometry::STGeomFromText('${polygon}', 4674);SELECT * FROM [SRH].[gisadmin].[INTERFERENCIA] WHERE @g.STContains(SHAPE) = 1`, function (err, recordset) {    
             if (err) console.log(err)
 
             // send records as a response
@@ -37,6 +48,8 @@ app.get('/', function (req, res) {
     });
 });
 
-var server = app.listen(5000, function () {
+app.use('/', express.static(public));
+
+app.listen(8080, function () {
     console.log('Server is running..');
 });
